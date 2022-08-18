900a1a8c2fd45e65fb459f0b1643a9fca63b0212
Firmin MAKABI <codeurlibremakabi@gmail.com>
2022-08-19 00:52:43 +0100

    ajout de la fonction runChronometre pour le chronometre de l'horloge

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import time
+
 temps_systeme = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 print(temps_systeme)
+
+def runChronometre():
+    reponse_user = input("Voulez-vous démarrer le chrono ? répondez par oui ou non : ")
+    if reponse_user == "oui":
+        for counter in range(0, 101):
+            print(counter + " seconde(s)")
+            time.sleep(1.0)
+
+runChronometre()
