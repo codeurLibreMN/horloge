cfb498e20286cc3199da172c4364c009933c3351
Firmin MAKABI <codeurlibremakabi@gmail.com>
2022-08-19 01:09:05 +0100

    correction d'une erreur de conversion dans la fonction runChronometre

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def runChronometre():
     reponse_user = input("Voulez-vous démarrer le chrono ? répondez par oui ou non : ")
     if reponse_user == "oui":
         for counter in range(0, 101):
-            print(counter + " seconde(s)")
+            print(str(counter) + " seconde(s)")
             time.sleep(1.0)
 
 runChronometre()
